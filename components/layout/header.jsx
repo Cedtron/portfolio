cf59e9a57453bba90dbc0f6026f0c2498c2bd5e9
Cedo <62746646+Cedtron@users.noreply.github.com>
2022-12-15 16:42:36 +0300

    test

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -10,7 +10,7 @@ export default function Header  (){
     return (
         <div>
             <Head>
-            <meta charset="utf-8"/>
+            <meta charSet="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<meta name="author" content="Cedric"/>
 	<link rel="shortcut icon" href="/image/favicon.png"/>
--- components/layout/nav.js
+++ components/layout/nav.js
@@ -15,7 +15,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Image from 'next/image'
 
-const pages = [<Link href="/">Home</Link>, <Link href="#about">About</Link>, <Link href="#service">Services</Link>,<Link href="#skills">Skills</Link>,<Link href="#contact">Contact</Link>];
+const pages = [<Link href="/">Home</Link>, <Link href="/#about">About</Link>, <Link href="/#service">Services</Link>,<Link href="/#skills">Skills</Link>,<Link href="/#contact">Contact</Link>];
 const settings = [<Link href="https://wa.me/256705465643">WhatsApp</Link>, <Link href="tel:256705465643">Call</Link>];
 
 export default function Navs() {
