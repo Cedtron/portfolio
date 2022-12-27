950dbf42ea1e9f2ca3c2dfd66389c58ddbe25ec7
Cedtron <62746646+Cedtron@users.noreply.github.com>
2022-12-27 23:07:58 +0300

    changing the about page

--- components/abouts.jsx
+++ components/abouts.jsx
@@ -12,25 +12,24 @@ export default function About ()  {
   <div className="untree_co-section" id="about">
     <div className="container">
       <div className="row">
-        <div className="col-lg-5" >
+        <div className="col-lg-6" >
         
             <Image  width={500}
       height={500} src="/images/pic1.png" alt="image" className="img-fluid rounded-20"/>
        
         </div>
-        <div className="col-lg-7 pl-lg-5 ml-auto neu pad">
+        <div className="col-lg-6 pl-lg-5 ml-auto neu pad">
           <h2 className="section-title mb-4">About My Self</h2>
-      <p>Hello! I’m Ssegirinya Cedric a self-taught &  Web Developer & Digital Designer  with over five years work experience.
+      <p>Hello! I’m Ssegirinya Cedric a self-taught &  Web Developer & Digital Designer  with over three years plus work experience.
 
 Love the Web and the work we do.I work closely with my clients to deliver the best possible solutions for their needs</p>
         <p>Well-organised person, problem solver, independent employee with high attention to detail. Fan of outdoor activities, TV series.</p>
         <h3>Education</h3>
         <p>I have a certificate in Computer Science and soon pursuing for a dipolam in Computer Science</p>
-        <h3>Education and skill set</h3>
+        <h3>Work experience</h3>
          <ul className="list-unstyled two-col clearfix">
-            <li>Certificate in Computer Science</li>
-            <li>Worked at Climaxx Microfinance as IT manager</li>
-            <li>MFI as implementation engineer</li>
+            <li>Climaxx Microfinance as IT manager 2021 -2022</li>
+            <li>MFI as implementation engineer 2022</li>
             
           </ul>
         </div>
