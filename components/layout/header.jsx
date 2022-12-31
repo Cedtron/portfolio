b06c42e2a815100804a501c300aecfc13b022195
Cedtron <62746646+Cedtron@users.noreply.github.com>
2022-12-31 15:16:38 +0300

    color theme change 2

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -14,7 +14,7 @@ export default function Header  (){
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<meta name="author" content="Cedric"/>
 	<link rel="shortcut icon" href="/image/favicon.png"/>
-    <meta name="theme-color" content="#dc3545" />
+    <meta name="theme-color" content="#0A1929" />
 	<meta name="description" content="Hello! I’m Ssegirinya Cedric a self-taught & Web Developer & Digital Designer  with over five years work experience." />
 	<meta name="keywords" content="Ssegirinya Cedric, Cedo IT,Cedo developer,Cedo tech,Cedo ug" />
     <title>Cedo Developer</title>
