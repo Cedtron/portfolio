a6a51af516d12749ec6d2451db360f878abdb1d2
Cedtron <62746646+Cedtron@users.noreply.github.com>
2023-01-02 09:40:33 +0300

    quick image load

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import "react-multi-carousel/lib/styles.css";
+import "../public/images/bg2.jpg"
+import "../public/images/bg.jpg"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
