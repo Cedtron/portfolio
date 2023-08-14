64e265afe9eff8e03d0527d226ad5bf42e6f1d65
Cedo Developer <62746646+Cedtron@users.noreply.github.com>
2023-08-14 05:24:28 +0300

    updated project
    i made image change according to screen size

--- components/proj.jsx
+++ components/proj.jsx
@@ -1,22 +1,12 @@
 import React, { useRef, useState } from 'react';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
 import Image from 'next/image';
 
 export default function Proj() {
 
-    const imageContainer = document.querySelector('img');
-    const projectDetails = document.querySelector('.project-details');
-    
-    imageContainer.addEventListener('mouseover', () => {
-      projectDetails.style.display = 'block';
-    });
-    
-    imageContainer.addEventListener('mouseout', () => {
-      projectDetails.style.display = 'none';
-    });
-
+  const [isHovered, setIsHovered] = useState(false);
+    const isMobile = typeof window !== 'undefined' && window.innerWidth <= 600;
   return (
    <div>
 
@@ -45,12 +35,14 @@ export default function Proj() {
             <div className="project-card">
         <Image
       // loader={myLoader}
-      src="/images/projects/excell.png"
-      alt="Picture of the cedo"
+       src={isMobile ? '/images/projects/excell2.png' : '/images/projects/excell.png'}
+      alt="Picture of the cedo"  
       width={800}
       height={420}
+     onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
     />
-    <div className="project-details">
+    <div className={`project-details ${isHovered ? 'visible' : ''}`}>
    Proudly designed and developed the official website for ExcelSolutions Uganda Ltd, 
     an import and export company facilitating seamless global trade.
    </div>
@@ -63,12 +55,14 @@ export default function Proj() {
             <div className="project-card">
         <Image
       // loader={myLoader}
-      src="/images/projects/jesus.png"
-      alt="Picture of the cedo"
+       src={isMobile ? '/images/projects/jesus2.png' : '/images/projects/jesus.png'}
+      alt="Picture of the cedo"  
       width={800}
       height={420}
+     onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
     />
-   <div className="project-details">I crafted and brought to life the JTM Church website,
+   <div className={`project-details ${isHovered ? 'visible' : ''}`}>I crafted and brought to life the JTM Church website,
     complete with an engaging online presence for their child charity organization,
     fostering a meaningful digital platform for their philanthropic initiatives.</div>
     <h3>Jesus the Messiah</h3>
@@ -80,12 +74,15 @@ export default function Proj() {
             <div className="project-card">
         <Image
       // loader={myLoader}
-      src="/images/projects/trecabb.png"
-      alt="Picture of the cedo"
+    
+       src={isMobile ? '/images/projects/trecabb2.png' : '/images/projects/trecabb.png'}
+      alt="Picture of the cedo"  
       width={800}
       height={420}
+     onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
     />
-   <div className="project-details">
+   <div className={`project-details ${isHovered ? 'visible' : ''}`}>
    Trecabb Environment Protection, effectively translating their commitment to environmental conservation
     into an impactful website that spreads awareness and encourages positive action.
    </div>
@@ -98,12 +95,15 @@ export default function Proj() {
             <div className="project-card">
         <Image
       // loader={myLoader}
-      src="/images/projects/gold.png"
-      alt="Picture of the cedo"
+
+       src={isMobile ? '/images/projects/gold2.png' : '/images/projects/gold.png'}
+      alt="Picture of the cedo"  
       width={800}
       height={420}
+     onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
     />
-   <div className="project-details"> played a pivotal role in designing and developing the Gateway Minerals website,
+   <div className={`project-details ${isHovered ? 'visible' : ''}`}> played a pivotal role in designing and developing the Gateway Minerals website,
     contributing to their online visibility as a key player in the mining industry</div>
    <h3>Gateway minerals</h3>
         </div>
